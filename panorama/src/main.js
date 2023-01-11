03a72d88ab4b9ab1d77175062affa4344074b0c1
gougebiegou42 <fanzhenyustudio@163.com>
2023-01-11 22:12:26 +0800

    first

---
+++ panorama/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+
+import App from './App.vue';
+import store from './store';
+import router from './router';
+
+import '@/style/index.less';
+import '@/assets/main.css';
+
+import '@/utils/flexible';
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
